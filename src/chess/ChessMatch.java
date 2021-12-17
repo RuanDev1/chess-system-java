5e9d5ca8e7cb75d9b6742f14c1f3c5eaa537f4d3
Ruan Rodrigues <rupriwell123@gmail.com>
2021-12-17 15:40:08 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,12 +9,25 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 	
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
 	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPleyer() {
+		return currentPlayer;
+	}
+	
 	public ChessPiece[][]getpieces(){
 		ChessPiece [][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for(int i=0;i<board.getRows(); i++) {
@@ -36,6 +49,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTagetPosition(source,target);
 		Piece capturedPiece = makeMove(source , target);
+		nextTurn();
 		return(ChessPiece)capturedPiece;
 	}
 	
@@ -50,6 +64,11 @@ public class ChessMatch {
 		if(!board.thereIsAPiace(position)) {
 			throw new ChessException("There is piece on source position");
 		}
+		
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
+		
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is possible moves for the chosen piece");
 		}
@@ -60,10 +79,13 @@ public class ChessMatch {
 			throw new ChessException("The chosen piece can 't move to target positio");
 		}
 	}
+	private void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK :Color.WHITE;
+	}
 	
 	private void placeNewPiece(char column,int row, ChessPiece piece) {
-		board.placePiece(piece, new ChessPosition(column, row).toPosition());
-		
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());		
 	}
 	
 	private void initialSetup() {
@@ -82,5 +104,3 @@ public class ChessMatch {
         placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
-
-
